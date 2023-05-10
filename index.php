f5562cddd17eb119fe6aa5afc3931b1afae13c04
mitsuun <lounge.attachment@gmail.com>
2023-05-10 22:32:33 +0900

    20230511_nagata_php04

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Status Code Quiz</title>
+    <link rel="styleseet" href="css/sanitize.css">
+    <link rel="styleseet" href="css/common.css">
+    <link rel="styleseet" href="css/index.css">
+</head>
+<body>
+  <header class="header">
+    <div class="header__inner">
+      <a class="header__logo" href="/php04/index.php">
+        Status Code Quiz
+      </a>
+    </div>
+  </header>  
+
+</body>
+</html>

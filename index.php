61da3d417fe1e8d048079c3f3398218d1bc750bb
mitsuun <lounge.attachment@gmail.com>
2023-05-11 17:46:33 +0900

    20230511_nagata_php04_2

--- index.php
+++ index.php
@@ -5,18 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Status Code Quiz</title>
-    <link rel="styleseet" href="css/sanitize.css">
-    <link rel="styleseet" href="css/common.css">
-    <link rel="styleseet" href="css/index.css">
+    <link rel="stylesheet" href="css/sanitize.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
   <header class="header">
     <div class="header__inner">
-      <a class="header__logo" href="/php04/index.php">
+      <a class="header__logo" href="/">
         Status Code Quiz
       </a>
     </div>
   </header>  
-
+  <main>
+    <div class="quiz__content">
+      <div class="question">
+        <p class="question__text">Q. 以下の内容に当てはまるステータスコードを選んでください</p>
+        <p class="question__text"></p>
+      </div>
+      <form class="quiz-form" action="result.php" method="post">
+        <input type="hidden" name="answer_code" value="">
+        <div class="quiz-form__item">
+          <div class="quiz-form__group">
+            <input class="quiz-form__radio" id="" type="radio" name="option" value="">
+            <label class="quiz-form__label" for="">
+            </label>
+          </div>
+        </div>
+        <div class="quiz-form__button">
+          <button class="quiz-form__button-submit" type="submit">
+            回答
+          </button>
+        </div>
+      </form>
+    </div>
+  </main>    
 </body>
 </html>
